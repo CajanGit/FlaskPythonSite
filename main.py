70dfe8e2f2f2abcc46fcd8e541af924ae0e8493b
Ethan <ethan.dvnn@gmail.com>
2025-03-21 01:23:47 -0700

    Reorganization

---
+++ main.py
@@ -0,0 +1,13 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#STOPPED AT 47:25 In https://www.youtube.com/watch?v=dam0GPOAvVI&t=175s&ab_channel=TechWithTim
+#STOPPED AT 47:25 In https://www.youtube.com/watch?v=dam0GPOAvVI&t=175s&ab_channel=TechWithTim
+#STOPPED AT 47:25 In https://www.youtube.com/watch?v=dam0GPOAvVI&t=175s&ab_channel=TechWithTim
+#STOPPED AT 47:25 In https://www.youtube.com/watch?v=dam0GPOAvVI&t=175s&ab_channel=TechWithTim
+#STOPPED AT 47:25 In https://www.youtube.com/watch?v=dam0GPOAvVI&t=175s&ab_channel=TechWithTim
